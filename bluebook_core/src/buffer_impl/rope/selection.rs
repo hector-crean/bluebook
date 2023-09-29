bbaaed4e908148d665f63a30feb3008149e227c1
Hector Crean <hectorcrean@gmail.com>
2023-09-29 18:07:27 +0100

    sync

--- bluebook_core/src/buffer_impl/rope/selection.rs
+++ bluebook_core/src/buffer_impl/rope/selection.rs
@@ -1,26 +1,28 @@
-use xi_rope::RopeDelta;
+use xi_rope::{RopeDelta, Transformer};
 
-use crate::selection::Selection;
+use crate::{
+    cursor::CursorRange,
+    selection::{InsertDrift, Selection},
+};
 
 impl Selection {
     pub fn apply_delta(&self, delta: &RopeDelta, after: bool, drift: InsertDrift) -> Selection {
         let mut result = Selection::new();
         let mut transformer = Transformer::new(delta);
-        for region in self.regions() {
-            let is_region_forward = region.start < region.end;
+        for cursor_range in self.cursor_ranges() {
+            let is_region_forward = cursor_range.anchor < cursor_range.head;
 
-            let (start_after, end_after) = match (drift, region.is_caret()) {
+            let (start_after, end_after) = match (drift, cursor_range.is_empty()) {
                 (InsertDrift::Inside, false) => (!is_region_forward, is_region_forward),
                 (InsertDrift::Outside, false) => (is_region_forward, !is_region_forward),
                 _ => (after, after),
             };
 
-            let new_region = SelRegion::new(
-                transformer.transform(region.start, start_after),
-                transformer.transform(region.end, end_after),
-                None,
+            let new_range = CursorRange::new(
+                transformer.transform(cursor_range.anchor, start_after),
+                transformer.transform(cursor_range.head, end_after),
             );
-            result.add_region(new_region);
+            result.add_curor_range(new_range);
         }
         result
     }
--- bluebook_core/src/selection.rs
+++ bluebook_core/src/selection.rs
@@ -1,9 +1,21 @@
-use crate::cursor::CursorRange;
+use crate::cursor::{self, CursorRange};
+
+/// Indicate whether a delta should be applied inside, outside non-caret selection or
+/// after a caret selection (see [`Selection::apply_delta`].
+#[derive(Copy, Clone)]
+pub enum InsertDrift {
+    /// Indicates this edit should happen within any (non-caret) selections if possible.
+    Inside,
+    /// Indicates this edit should happen outside any selections if possible.
+    Outside,
+    /// Indicates to do whatever the `after` bool says to do
+    Default,
+}
 
 /// A selection holding one or more [`SelRegion`].
 #[derive(Clone, PartialEq, Debug, serde::Serialize, serde::Deserialize)]
 pub struct Selection {
-    regions: Vec<CursorRange>,
+    cursor_ranges: Vec<CursorRange>,
     last_inserted: usize,
 }
 
@@ -16,14 +28,16 @@ impl AsRef<Selection> for Selection {
 impl Selection {
     pub fn new() -> Selection {
         Selection {
-            regions: Vec::new(),
+            cursor_ranges: Vec::new(),
             last_inserted: 0,
         }
     }
 
+    pub fn add_curor_range(cursor_range: CursorRange) -> Selection {}
+
     pub fn contains(&self, offset: usize) -> bool {
-        for region in self.regions.iter() {
-            if region.contains(offset) {
+        for cursor_range in self.cursor_ranges.iter() {
+            if cursor_range.contains(offset) {
                 return true;
             }
         }
@@ -31,6 +45,12 @@ impl Selection {
     }
 
     pub fn cursor_ranges(&self) -> &[CursorRange] {
-        &self.regions
+        &self.cursor_ranges
+    }
+
+    /// A [`Selection`] is considered to be a caret if it contains
+    /// only caret [`SelRegion`] (see [`SelRegion::is_caret`])
+    pub fn is_point(&self) -> bool {
+        self.cursor_ranges.iter().all(|r| r.is_empty())
     }
 }
